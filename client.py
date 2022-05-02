9329559707a823cf3caea3e79e747fbebc9b5629
João Pedro <joaopedro1santiago@gmail.com>
2022-05-02 19:49:27 -0300

    Formatando o código com black formatter.

--- client.py
+++ client.py
@@ -1,60 +1,71 @@
-from flask import Flask, render_template, request, redirect, flash, url_for
-import requests
 import secrets
+
+import requests
+from flask import Flask, flash, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
 secret = secrets.token_urlsafe(32)
 app.secret_key = secret
 
-headers = {'Content-Type': "application/json", 'Accept': "application/json"}
+headers = {"Content-Type": "application/json", "Accept": "application/json"}
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    response = requests.get('https://api-modelos-ia.herokuapp.com/modelo', headers=headers)
+    response = requests.get(
+        "https://api-modelos-ia.herokuapp.com/modelo", headers=headers
+    )
     if response.status_code == 200:
         modelos = response.json()
-        return render_template('index.html', modelos=modelos)
+        return render_template("index.html", modelos=modelos)
 
-@app.route('/create', methods=['POST'])
+
+@app.route("/create", methods=["POST"])
 def create():
-    nome = request.form['nome']
-    descricao = request.form['descricao']
+    nome = request.form["nome"]
+    descricao = request.form["descricao"]
 
-    requests.post('https://api-modelos-ia.herokuapp.com/modelo', json={'nome':nome, 'descricao': descricao}, headers=headers)
+    requests.post(
+        "https://api-modelos-ia.herokuapp.com/modelo",
+        json={"nome": nome, "descricao": descricao},
+        headers=headers,
+    )
 
     flash("Modelo adicionado com sucesso!")
 
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
 
-@app.route('/update', methods=['GET','POST'])
+
+@app.route("/update", methods=["GET", "POST"])
 def update():
     if request.method == "POST":
-        id = request.form['id']
+        id = request.form["id"]
 
-        nome = request.form['nome']
-        descricao = request.form['descricao']
+        nome = request.form["nome"]
+        descricao = request.form["descricao"]
 
-        link = 'https://api-modelos-ia.herokuapp.com/modelo/{0}'.format(id)
+        link = "https://api-modelos-ia.herokuapp.com/modelo/{0}".format(id)
 
-        requests.put(link, json={'nome':nome, 'descricao': descricao}, headers=headers)
+        requests.put(link, json={"nome": nome, "descricao": descricao}, headers=headers)
 
         flash("Modelo editado com sucesso!")
-        
-        return redirect(url_for('index'))
+
+        return redirect(url_for("index"))
     else:
-        redirect(url_for('delete'))
+        redirect(url_for("delete"))
 
 
-@app.route('/delete/<int:id>')
+@app.route("/delete/<int:id>")
 def delete(id):
-    link = 'https://api-modelos-ia.herokuapp.com/delete/{0}'.format(id)
+    link = "https://api-modelos-ia.herokuapp.com/delete/{0}".format(id)
 
     requests.delete(link, headers=headers)
 
     flash("Modelo Deletado com Sucesso.")
 
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
+
 
 if __name__ == "__main__":
     app.run(port=3000)
